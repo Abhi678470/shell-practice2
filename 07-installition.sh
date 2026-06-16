1ce8ae60dcd7c65fc5632147ffa247bc842820df
Abhilash Kanchi <kanchiabhilash@kyndryl.com>
2026-06-16 13:26:28 +0530

    learning

--- 07-installition.sh
+++ 07-installition.sh
@@ -4,16 +4,18 @@ USERID=$(id -u)
 
 if [$USERID -ne 0 ];then
     echo "please login with the root credentionals"
+    exit 1
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0]; then
     echo "installing my sql is failure"
+    exit 1
 else
     echo "installing mysql is success"
 fi
-    
+
 
 
 
